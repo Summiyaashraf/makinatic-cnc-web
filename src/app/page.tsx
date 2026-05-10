f98452046f780966e33c49a3696fa628cb14fb8e
Summiya Ashraf <168761262+Summiyaashraf@users.noreply.github.com>
2026-05-10 22:22:53 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import dynamic from 'next/dynamic'; // Dynamic import ko use karein
 import Navbar from "@/src/app/components/Navbar";
 import Hero from "@/src/app/components/Hero";
 import ProductSection from "@/src/app/components/Products";
@@ -5,7 +6,11 @@ import AboutSection from "@/src/app/components/About";
 import Footer from "@/src/app/components/Footer";
 import Features from "@/src/app/components/Features";
 import ContactPage from "@/src/app/components/Contact";
-import ReviewsPage from "./components/Review";
+
+const ReviewsPage = dynamic(() => import("./components/Review"), { 
+  ssr: false,
+  loading: () => <div className="h-20 bg-gray-50 flex items-center justify-center italic text-gray-400">Loading Reviews...</div>
+});
 
 export default function Home() {
   return (
@@ -15,7 +20,9 @@ export default function Home() {
       <ProductSection />
       <AboutSection />
       <Features />
+      
       <ReviewsPage />
+      
       <ContactPage />
       <Footer />
     </main>
