d2f6ac82f0491a6fa86ef5fce7d936299c95ed67
Summiya Ashraf <168761262+Summiyaashraf@users.noreply.github.com>
2026-05-10 22:14:09 +0500

    Update page.tsx

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,22 +1,13 @@
 import Navbar from "@/src/app/components/Navbar";
 import Footer from "@/src/app/components/Footer";
 import ProductSection from "@/src/app/components/Products";
-import { Suspense } from "react";
 
 export default function AllProductsPage() {
   return (
     <main className="min-h-screen bg-white">
       <Navbar />
       <div className="pt-10"> 
-        <Suspense fallback={
-          <div className="min-h-[50vh] flex items-center justify-center bg-[#061a80]">
-            <div className="text-white font-bold animate-pulse uppercase tracking-widest">
-              Loading Machines...
-            </div>
-          </div>
-        }>
-          <ProductSection isFullPage={true} />
-        </Suspense>
+        <ProductSection isFullPage={true} />
       </div>
       <Footer />
     </main>
