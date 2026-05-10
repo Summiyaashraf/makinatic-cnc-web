24be3148aa8657251d41cda3de84c84919d7171a
Summiya Ashraf <168761262+Summiyaashraf@users.noreply.github.com>
2026-05-10 22:15:20 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {/* Poore app ko provider mein wrap kar diya */}
+      
         <LanguageProvider>
           {children}
           <FloatingWhatsApp />
