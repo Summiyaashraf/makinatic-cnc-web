c20228f81778563aebc76daec6274625167c4b29
Summiya Ashraf <168761262+Summiyaashraf@users.noreply.github.com>
2026-05-12 21:58:57 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+"use client"; 
 import React, { Suspense } from "react";
 import Navbar from "@/src/app/components/Navbar";
 import Hero from "@/src/app/components/Hero";
@@ -9,7 +9,7 @@ import Features from "@/src/app/components/Features";
 import ContactPage from "@/src/app/components/Contact";
 import ReviewSection from "./components/Review";
 import MaintenanceSection from "./components/Maintenance";
-import { useLanguage } from "./context/LanguageContext";
+import { useLanguage } from "./context/LanguageContext"; 
 
 export default function Home() {
   const context = useLanguage();
@@ -18,13 +18,11 @@ export default function Home() {
 
   return (
     <main className="min-h-screen bg-white">
-      <Suspense fallback={<div className="h-screen flex items-center justify-center">Loading...</div>}>
+      <Suspense fallback={<div className="h-screen flex items-center justify-center text-blue-900">Loading...</div>}>
         <Navbar />
         <Hero /> 
         <ProductSection />
-
         <MaintenanceSection isAr={isAr} />
-        
         <AboutSection />
         <Features />
         <ReviewSection />
