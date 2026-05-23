8bee24636ace93e671f78e1ba2b623a8e36af003
Summiya Ashraf <168761262+Summiyaashraf@users.noreply.github.com>
2026-05-24 00:09:12 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { LanguageProvider } from "./context/LanguageContext";
 import FloatingWhatsApp from "./components/FloatingWhatsApp";
+import Script from "next/script"; // NEXT.JS SCRIPT IMPORT KIYA
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,8 +27,36 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* === Meta Pixel Code START === */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1883653982347259');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        
+        {/* Noscript fallback background image for users with disabled javascript */}
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{ display: 'none' }} 
+            src="https://www.facebook.com/tr?id=1883653982347259&ev=PageView&noscript=1" 
+            alt="meta pixel tracking fallback"
+          />
+        </noscript>
+        {/* === Meta Pixel Code END === */}
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-      
         <LanguageProvider>
           {children}
           <FloatingWhatsApp />
